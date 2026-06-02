e56e76feabf39163e6034ff6bb4e25e79b369486
Julian Meñaca <jsmenaca@gmail.com>
2026-06-02 10:41:54 -0500

    cambios del login, campo de pin y botón de volver al menu inicial

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>CSS Glassmorphism | @codingstella</title>
+  <title>Iniciar sesión</title>
   <link rel="stylesheet" href="../proyecto_asistecia/style/loginstyle.css">
 
 </head>
@@ -12,14 +12,14 @@
 
   <div class="login-box">
     <form action="">
-      <h2>Login</h2>
+      <h2>Iniciar sesión</h2>
 
       <div class="input-box">
         <span class="icon">
           <ion-icon name="mail"></ion-icon>
         </span>
         <input type="email" required>
-        <label>Email</label>
+        <label>Documento</label>
       </div>
 
       <div class="input-box">
@@ -27,19 +27,20 @@
           <ion-icon name="lock-closed"></ion-icon>
         </span>
         <input type="password" required>
-        <label>Password</label>
+        <label>PIN </label>
       </div>
 
-      <div class="remember-forgot">
-        <label><input type="checkbox"> Remember me</label>
-        <a href="#">Forgot Password?</a>
+       <div class="input-box">
+        <span class="icon">
+          <ion-icon name="lock-closed"></ion-icon>
+        </span>
+        <input type="password" required>
+        <label>Contraseña</label>
       </div>
 
-      <button type="submit">Login</button>
-
-      <div class="register-link">
-        <p>Don't have an account? <a href="#">Register</a></p>
-      </div>
+      <button type="submit" class="btn-login">Login</button>
+      <button type="submit">Volver al inicio</button>
+      
     </form>
   </div>
 
